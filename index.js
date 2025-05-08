ef9a9aef9c28a22d42e51d3c73fca0870903385a
minji105 <ajhjmj9@naver.com>
2025-05-08 18:03:39 +0900

    미션 3

---
+++ index.js
@@ -0,0 +1,23 @@
+const buttons = document.getElementsByClassName('button');
+const display = document.getElementById('display');
+
+Array.from(buttons).forEach(button => {
+  button.addEventListener('click', () => {
+    const value = button.value;
+    const current = display.textContent;
+
+    console.log(value);
+
+    if (value === 'C') {
+      display.textContent = '0';
+      return;
+    }
+
+    if (button.classList.contains('number')) {
+      display.textContent = current === '0' ? value : current + value;
+    }
+
+    if (value === '.' && !display.textContent.includes('.'))
+      display.textContent += value;
+  })
+});
